0b67a4ce80bea24caa452881f6b6a45b22bacc97
WonSeok Jeong <g0401828t@gmail.com>
2022-01-11 21:54:03 +0900

    Create 시험성적.py

---
+++ if문/시험성적.py
@@ -0,0 +1,15 @@
+score = int(input())
+
+if score >= 60:
+    if score >= 70:
+        if score >= 80:
+            if score >= 90:
+                print("A")
+            else:
+                print("B")
+        else:
+            print("C")
+    else:
+        print("D")
+else:
+    print("F")
