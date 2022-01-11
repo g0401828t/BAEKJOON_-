9661bc723a91df83d2114dafb501e4b44d9b6727
WonSeok Jeong <g0401828t@gmail.com>
2022-01-11 21:57:48 +0900

    Create 빠른 A+B.py

---
+++ for문/빠른 A+B.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+result = 0
+for n in range(1, N+1):
+    result += n
+
+print(result)
+    
+# import sys
+
+# for t in  range(int(input())):
+#     A, B = map(int, sys.stdin.readline().split())
+#     print(A + B)
