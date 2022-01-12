bb6a43a5ecb08ff14246c8c07d0cfba2ef9c7818
WonSeok Jeong <g0401828t@gmail.com>
2022-01-12 21:49:42 +0900

    Create 셀프 넘버.py

---
+++ 함수/셀프 넘버.py
@@ -0,0 +1,23 @@
+# # 1st trail -> 시간초과
+# def solve():
+#     for i in range(1, 10001):
+        
+#         thereis = 0
+#         for a in range(10):
+#             for b in range(10):
+#                 for c in range(10):
+#                     for d in range(10):
+#                         d = 1001*d + 101*c + 11*b + 2*a
+
+#                         if d == i:
+#                             thereis = 1
+#                     if thereis == 1:
+#                         break
+#                 if thereis == 1:
+#                     break
+#             if thereis == 1:
+#                 break
+        
+#         if thereis == 0:
+#             print(i)
+
