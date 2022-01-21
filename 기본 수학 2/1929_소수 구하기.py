5687c1cb0e991b77b3fbd9d16422e7cbbdbbdb0e
WonSeok Jeong <g0401828t@gmail.com>
2022-01-21 22:29:31 +0900

    Update 1929_소수 구하기.py

--- 기본 수학 2/1929_소수 구하기.py
+++ 기본 수학 2/1929_소수 구하기.py
@@ -70,3 +70,18 @@ for i in range(M, N+1):
         print(i)
 
         
+# # 2nd Sol
+m, n = map(int, input().split())
+
+def isprime(m, n):
+  prime = [True] * n                # n개의 [True]가 있는 리스트 생성
+  for i in range(2, n+1):
+    if prime[i]:                    # prime[i]가 True일때
+      for j in range(2*i, n, i):    # prime 내 i의 배수들을 False로 변환
+        prime[j] = False
+
+  for i in range(m, n):
+    if i > 1 and prime[i] == True:  # 1 이상이면서 남은 소수들을 출력
+      print(i)
+
+isprime(m, n)
