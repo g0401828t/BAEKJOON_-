aceec4fd0ae2c12a6b0b0e8f07d8f9f5aeeb188d
WonSeok Jeong <g0401828t@gmail.com>
2022-01-24 21:58:16 +0900

    Update 4948_베르트랑 공존.py

--- 기본 수학 2/4948_베르트랑 공존.py
+++ 기본 수학 2/4948_베르트랑 공존.py
@@ -24,3 +24,34 @@ while True:
       count += 1
   print(count)
 
+# # 1st Sol:
+import sys
+
+def howmany_sosu(X):
+  # [X+1, X+2, ... , 2X]
+  # [2, 3, 4, 5, ..., 2X]
+  data = [True] * (2*X - 1)
+
+  for x in range(2, 2*X + 1):
+    if data[x-2] == True:
+      for i in range(2*x, 2*X + 1, x):
+        # print("i:", i)
+        data[i-2] = False
+        # print(data) 
+  
+  count = 0
+  for d in range(X-1, len(data)):
+    if data[d] == True:
+      count += 1  
+  return count
+
+while True:
+  n = int(sys.stdin.readline())
+
+  if n == 0:
+    break
+  if n == 1:
+    print(1)
+  else:
+    result = howmany_sosu(n)
+    print(result)
