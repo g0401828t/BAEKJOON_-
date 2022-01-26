0a9f1f11cbb582d5f1c12cf8dcb5af9162ab1a81
WonSeok Jeong <g0401828t@gmail.com>
2022-01-26 21:09:04 +0900

    Update 9020_골드바흐.py

--- 기본 수학 2/9020_골드바흐.py
+++ 기본 수학 2/9020_골드바흐.py
@@ -1,44 +1,79 @@
 # # 1st Trial => 시간 초과
-import math
-import sys
-from itertools import product
+# import math
+# import sys
+# from itertools import product
 
-def Sosu_list(X):
-  data = [True] * X
-  sosu_list = []
-  for x in range(1, int(X**0.5)+1):
-    if x == 1:
-      data[x-1] = False
-    elif data[x-1] == True:
-      for i in range(2*x, X+1, x):
-        data[i-1] = False
+# def Sosu_list(X):
+#   data = [True] * X
+#   sosu_list = []
+#   for x in range(1, int(X**0.5)+1):
+#     if x == 1:
+#       data[x-1] = False
+#     elif data[x-1] == True:
+#       for i in range(2*x, X+1, x):
+#         data[i-1] = False
   
-  for d in range(len(data)):
-    if data[d] == True:
-      sosu_list.append(d+1)
+#   for d in range(len(data)):
+#     if data[d] == True:
+#       sosu_list.append(d+1)
 
-  return sosu_list
+#   return sosu_list
 
-T = int(sys.stdin.readline())
+# T = int(sys.stdin.readline())
 
-for t in range(T):
-  n = int(sys.stdin.readline())
+# for t in range(T):
+#   n = int(sys.stdin.readline())
 
-  sosu_list = Sosu_list(n)
-  print(sosu_list)
+#   sosu_list = Sosu_list(n)
+#   print(sosu_list)
   
-  diff = math.inf
-  for s in sosu_list:
-    for o in sosu_list:
-      if s+o == n and abs(s-o) < diff:
-        result1, result2 = s, o
-        diff = abs(s-o)
+#   diff = math.inf
+#   for s in sosu_list:
+#     for o in sosu_list:
+#       if s+o == n and abs(s-o) < diff:
+#         result1, result2 = s, o
+#         diff = abs(s-o)
 
-  # # 중복 순열도 시간 초과....
-  # diff = math.inf
-  # for i in product(sosu_list, sosu_list):
-  #   if sum(i) == n and abs(i[0] - i[1]) < diff:
-  #     result1, result2 = i[0], i[1]
-  #     diff = abs(i[0] - i[1]) 
+#   # # 중복 순열도 시간 초과....
+#   # diff = math.inf
+#   # for i in product(sosu_list, sosu_list):
+#   #   if sum(i) == n and abs(i[0] - i[1]) < diff:
+#   #     result1, result2 = i[0], i[1]
+#   #     diff = abs(i[0] - i[1]) 
 
-  print(result1, result2)
+#   print(result1, result2)
+
+# # 2nd Trial: sosu list 찾으면서 바로 구하기 => 시간초과
+# import math
+# import sys
+# from itertools import product
+
+# def Sosu_list(X):
+#   data = [True] * X
+#   sosu_list = []
+#   for x in range(1, int(X**0.5)+1):
+#     if x == 1:
+#       data[x-1] = False
+#     elif data[x-1] == True:
+#       for i in range(2*x, X+1, x):
+#         data[i-1] = False
+  
+#   diff = math.inf
+#   for d in range(len(data)):
+#     for b in range(len(data)):
+#       if data[d] == True and data[b] == True:
+#         # print(d+1, b+1)
+#         if (d+1) + (b+1) == X and abs((d+1) - (b+1)) < diff:
+#           result1, result2 = d+1, b+1
+#           diff = abs((d+1) - (b+1))
+
+#   return result1, result2
+
+# T = int(sys.stdin.readline())
+
+# for t in range(T):
+#   n = int(sys.stdin.readline())
+
+#   result1, result2 = Sosu_list(n)
+
+#   print(result1, result2)
