7d5a5b488d427f00823c385e4258c621c2b05f70
WonSeok Jeong <g0401828t@gmail.com>
2022-02-24 22:06:17 +0900

    Create 2751_수 정렬하기 2.py

---
+++ 정렬/2751_수 정렬하기 2.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+data = []
+for _ in range(N):
+    data.append(int(sys.stdin.readline()))
+
+data.sort()
+
+for i in data:
+    # print(i)
+    sys.stdout.write(str(i)+'\n')
